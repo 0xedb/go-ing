848f64b49017a78da8fa958ab59ac9e9e285fe6e
theBashShell <edohbruno@gmail.com>
2019-12-28 14:59:05 +0000

    more experiments

--- src/hello.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var age = 3
-	fmt.Println("hello world", age)
-
-}
---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	cout := 10
+	for cout < 100 {
+		cout += 1
+		fmt.Println(cout)
+	}
+
+}
