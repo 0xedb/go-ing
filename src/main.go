12ae2d0cee4bcf8cd73149597fe0e7959f1b2fb9
theBashShell <edohbruno@gmail.com>
2019-12-29 12:54:47 +0000

    goroutine

--- src/main.go
+++ src/main.go
@@ -1,27 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-type IPAddr [4]byte
-
-// TODO: Add a "String() string" method to IPAddr.
-func (ip IPAddr) String() string {
-	var str string
-	for i := 0; i < len(ip); i++ {
-		str += fmt.Sprintf("%v", ip[i])
-		if i != len(ip)-1 {
-			str += "."
-		}
+func printer(msg string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(msg)
+		time.Sleep(100 * time.Microsecond)
 	}
-	return str
 }
 
 func main() {
-	hosts := map[string]IPAddr{
-		"loopback":  {127, 0, 0, 1},
-		"googleDNS": {8, 8, 8, 8},
-	}
-	for name, ip := range hosts {
-		fmt.Printf("%v: %v\n", name, ip)
-	}
+	go printer("first")
+	printer("second")
+
 }
