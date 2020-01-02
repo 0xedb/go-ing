82c00aa36ab6f5c20979ea19be09dc195d4389d9
theBashShell <edohbruno@gmail.com>
2020-01-02 12:00:07 +0000

    init function

--- src/main.go
+++ src/main.go
@@ -2,7 +2,24 @@ package main
 
 import "fmt"
 
+func init() {
+	fmt.Println("Doing this first")
+}
+
+func init() {
+	fmt.Println("Doing this second")
+}
+
+func one() int {
+	return 1
+}
+
+func zero() int {
+	return 60
+}
+
 func main() {
 	w := 'K'
 	fmt.Printf("%T", w)
+ 
 }
