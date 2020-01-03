c8571343a05514d144a1ba12ad97ac64433c0fb8
theBashShell <edohbruno@gmail.com>
2020-01-03 11:08:58 +0000

    interfaces

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -7,8 +7,26 @@ import (
 	"github.com/theBashShell/beautiful"
 )
 
+type Human struct {
+	name string
+	age uint16
+}
+
+type Walk interface {
+	Walking()
+}
+
+func (h Human) Walk() {
+	fmt.Printf("%s is walking...", h.name)
+}
+
 func main() {
 	fmt.Println("main her!!!!!e")
 	fmt.Println(beautiful.Doubler(339))
 	fmt.Println(stringutil.Reverse("love"))
+
+	bruno := Human{"bruno", 1}
+	fmt.Println(bruno)
+	bruno.Walk()
+
 }
