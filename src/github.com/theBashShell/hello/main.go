e72d9ed819b89b40aae9e3e0b89fc3ec40f207a6
theBashShell <edohbruno@gmail.com>
2020-01-03 00:49:57 +0000

    remote packages

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -1,12 +1,14 @@
 package main
 
-import "fmt"
-// import "github.com/theBahsShell/beautiful"
-import "github.com/theBashShell/beautiful"
- 
+import (
+	"fmt"
+
+	"github.com/golang/example/stringutil"
+	"github.com/theBashShell/beautiful"
+)
 
 func main() {
 	fmt.Println("main her!!!!!e")
-	// beautiful.Doubler(3932)
 	fmt.Println(beautiful.Doubler(339))
+	fmt.Println(stringutil.Reverse("love"))
 }
