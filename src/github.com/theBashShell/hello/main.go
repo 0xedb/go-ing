008579db25cc162ba1d350b63508febcf7324a8d
theBashShell <edohbruno@gmail.com>
2020-01-19 12:02:53 +0000

    producer consumer example

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -5,20 +5,32 @@ import (
 	"sync"
 )
 
-func conCur(msg string, wg *sync.WaitGroup) {
+func producer(ch chan string, wg *sync.WaitGroup) {
 	wg.Add(1)
 	go func() {
-		for st := 1; st <= 1000; {
-			fmt.Println(msg, "-----", st)
-			st++
-		}
-		wg.Done()
+
+		fmt.Println("producer")
+		ch <- "producer"
+
+		defer wg.Done()
+	}()
+}
+
+func consumer(ch chan string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+
+		received := <-ch
+		fmt.Println("consumed:\t", received)
+
+		defer wg.Done()
 	}()
 }
 
 func main() {
 	var wg sync.WaitGroup
-	conCur("bruno", &wg)
-	conCur("Kofi", &wg)
+	channel := make(chan string)
+	producer(channel, &wg)
+	consumer(channel, &wg)
 	wg.Wait()
 }
