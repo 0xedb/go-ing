8fceaca8af10a08337937b520d108714302fb08c
theBashShell <edohbruno@gmail.com>
2020-01-19 21:29:20 +0000

    not not

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -5,32 +5,33 @@ import (
 	"sync"
 )
 
-func producer(ch chan string, wg *sync.WaitGroup) {
+func producer(ch chan int, wg *sync.WaitGroup, slice *[]int) {
 	wg.Add(1)
 	go func() {
-
-		fmt.Println("producer")
-		ch <- "producer"
-
+		for i := 0; i < 10; i++ {
+			ch <- i 
+		}
+		close(ch)
 		defer wg.Done()
 	}()
+	defer fmt.Println(*slice)
 }
 
-func consumer(ch chan string, wg *sync.WaitGroup) {
+func consumer(ch chan int, wg *sync.WaitGroup, mut *sync.Mutex) {
 	wg.Add(1)
 	go func() {
-
-		received := <-ch
-		fmt.Println("consumed:\t", received)
-
+		for i := range ch {
+			fmt.Printf("***\tconsumed:\t%v\n", i)
+		}
 		defer wg.Done()
 	}()
 }
 
 func main() {
 	var wg sync.WaitGroup
-	channel := make(chan string)
-	producer(channel, &wg)
-	consumer(channel, &wg)
+	var slice []int
+
+	channel := make(chan int)
+	producer(channel, &wg, &slice) 
 	wg.Wait()
 }
