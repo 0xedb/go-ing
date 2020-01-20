954eece51cc58c2f6d16ae769d55c2a061a43b27
theBashShell <edohbruno@gmail.com>
2020-01-20 02:06:12 +0000

    go new keyword

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -2,36 +2,16 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func producer(ch chan int, wg *sync.WaitGroup, slice *[]int) {
-	wg.Add(1)
-	go func() {
-		for i := 0; i < 10; i++ {
-			ch <- i 
-		}
-		close(ch)
-		defer wg.Done()
-	}()
-	defer fmt.Println(*slice)
-}
-
-func consumer(ch chan int, wg *sync.WaitGroup, mut *sync.Mutex) {
-	wg.Add(1)
-	go func() {
-		for i := range ch {
-			fmt.Printf("***\tconsumed:\t%v\n", i)
-		}
-		defer wg.Done()
-	}()
+// ListNode for linkedlist
+type ListNode struct {
+	data string
+	next *ListNode
 }
 
 func main() {
-	var wg sync.WaitGroup
-	var slice []int
-
-	channel := make(chan int)
-	producer(channel, &wg, &slice) 
-	wg.Wait()
+	linkedList := new(ListNode)
+	fmt.Println(linkedList.data, linkedList.next)
+	fmt.Printf("%T\b", linkedList)
 }
