7a56e65b9354a4747f40f6ed6f6a5397976c16a3
theBashShell <edohbruno@gmail.com>
2020-02-04 23:22:54 +0000

    advanced channels

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func putter(w int, ch *chan int) {
 	*ch <- w
+	close(*ch)
 }
 
 func main() {
 	ch := make(chan int)
-	go putter(200, &ch)
+	go putter(27800, &ch)
 
-	w := <-ch
-	fmt.Println(w)
+	for i := range ch {
+		fmt.Println("-----", i)
+	}
 }
