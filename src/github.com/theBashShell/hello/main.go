418757b59da3914e30813089babc965bfb927d63
theBashShell <edohbruno@gmail.com>
2020-02-04 16:20:49 +0000

    more methods

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -3,6 +3,8 @@ package main
 
 import "fmt"
 
+type str string
+
 /*Human is a human prototype */
 type Human struct {
 	name string
@@ -14,11 +16,17 @@ func (h *Human) info() string {
 	return fmt.Sprintf("%s----", h.name)
 }
 
+func (st str) more() {
+	fmt.Println(st)
+}
+
 func main() {
 	bruno := new(Human)
 	bruno.name = "bruno kofi edoh"
+	var ss str = "bru!"
 	var myAge uint16 = 200
 	fmt.Println(myAge)
 	fmt.Println(bruno)
 	fmt.Println(bruno.info())
+	ss.more()
 }
