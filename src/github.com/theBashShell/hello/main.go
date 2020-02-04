1f535c1f81fb733c74f24b611f42528d7f332957
theBashShell <edohbruno@gmail.com>
2020-02-04 02:58:20 +0000

    pointers

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -3,7 +3,17 @@ package main
 
 import "fmt"
 
+/*Human is a human prototype */
+type Human struct {
+	name string
+	age uint16
+	isMarried bool
+}
+
 func main() {
+	bruno := new(Human)
+	bruno.name = "bruno kofi edoh"
 	var myAge uint16 = 200
 	fmt.Println(myAge)
+	fmt.Println(bruno)
 }
