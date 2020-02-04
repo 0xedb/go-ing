d9708dfa0927467e98945f763ae833f5c7a5b432
theBashShell <edohbruno@gmail.com>
2020-02-04 23:35:06 +0000

    channel selection

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -1,17 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "time"
+)
 
-func putter(w int, ch *chan int) {
-	*ch <- w
+func putter(ch *chan int) {
+	*ch <- 1
+	close(*ch)
+}
+
+func another(ch *chan int) {
+	*ch <- 2
+	// time.Sleep(time.Millisecond * 100)
 	close(*ch)
 }
 
 func main() {
 	ch := make(chan int)
-	go putter(27800, &ch)
+	ch2 := make(chan int)
 
-	for i := range ch {
-		fmt.Println("-----", i)
+	go putter(&ch)
+	go another(&ch2)
+
+	select {
+	case one := <-ch:
+		fmt.Println("channel 1: ", one)
+	case two := <-ch2:
+		fmt.Println("channel 2: ", two)
 	}
+
 }
