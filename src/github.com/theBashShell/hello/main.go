f1f9a8a3f9f628642cbde6eea53f9881421c887e
theBashShell <edohbruno@gmail.com>
2020-02-04 16:16:09 +0000

    methods

--- src/github.com/theBashShell/hello/main.go
+++ src/github.com/theBashShell/hello/main.go
@@ -10,10 +10,15 @@ type Human struct {
 	isMarried bool
 }
 
+func (h *Human) info() string {
+	return fmt.Sprintf("%s----", h.name)
+}
+
 func main() {
 	bruno := new(Human)
 	bruno.name = "bruno kofi edoh"
 	var myAge uint16 = 200
 	fmt.Println(myAge)
 	fmt.Println(bruno)
+	fmt.Println(bruno.info())
 }
